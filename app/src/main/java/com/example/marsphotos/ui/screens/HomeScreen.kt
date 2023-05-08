6eac7c751be5624cb6be67c94d64533fe52d497a
Ryuji Koyama <ryujikoyama95@gmail.com>
2023-05-08 22:20:20 +0900

    Add LoadingScreen

--- app/src/main/java/com/example/marsphotos/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/marsphotos/ui/screens/HomeScreen.kt
@@ -15,12 +15,14 @@
  */
 package com.example.marsphotos.ui.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.marsphotos.R
@@ -35,6 +37,20 @@ fun HomeScreen(
         is MarsUiState.Success -> ResultScreen(marsUiState.photos, modifier)
         is MarsUiState.Loading -> LoadingScreen(modifier)
         is MarsUiState.Error -> ErrorScreen(modifier)
+    }
+}
+
+@Composable
+fun LoadingScreen(modifier: Modifier = Modifier) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = modifier.fillMaxSize()
+    ) {
+        Image(
+            modifier = modifier,
+            painter = painterResource(id = R.drawable.loading_img),
+            contentDescription = stringResource(id = R.string.loading)
+        )
     }
 }
 
