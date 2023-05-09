dafaeec746247ac1baf6527a4c5ae6509f6346e8
Ryuji Koyama <ryujikoyama95@gmail.com>
2023-05-09 09:12:02 +0900

    Fix param of addConverterFactory

--- app/src/main/java/com/example/marsphotos/network/MarsApiService.kt
+++ app/src/main/java/com/example/marsphotos/network/MarsApiService.kt
@@ -1,14 +1,16 @@
 package com.example.marsphotos.network
 
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType
 import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.GET
 
 private const val BASE_URL = "https://android-kotlin-fun-mars-server.appspot.com"
 
 private val retrofit = Retrofit
     .Builder()
-    .addConverterFactory(ScalarsConverterFactory.create()) // JSON結果をStringやプリミティブ型に変換
+    .addConverterFactory(Json.asConverterFactory(MediaType.get("application/json")))
     .baseUrl(BASE_URL) // URLを設定
     .build()
 
