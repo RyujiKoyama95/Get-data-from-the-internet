4f7312283973e1fad5b66c9685092bd43248f245
Ryuji Koyama <ryujikoyama95@gmail.com>
2023-05-09 09:07:11 +0900

    Add data class MarsPhoto

---
+++ app/src/main/java/com/example/marsphotos/network/MarsPhoto.kt
@@ -0,0 +1,14 @@
+package com.example.marsphotos.network
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+// シリアル化可能にするため、@Serializableをつける
+// JSONオブジェクトのキー名と対応するように変数名をつける
+// kotlin規則的に変数名はキャメルケースを使用なので、@SerialNameでキー名を指定して紐づける
+@Serializable
+data class MarsPhoto(
+    val id: String,
+    @SerialName(value = "img_src")
+    val imgSrc: String
+)
